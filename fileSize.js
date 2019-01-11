2034ae53b615ec6307a1338477726b9273ed6fe1
liang7 <721689246@qq.com>
2019-01-11 16:58:22 +0800

    优化

--- fileSize.js
+++ fileSize.js
@@ -1,16 +1,16 @@
 const fs = require('fs')
 const path = require('path')
-
-const folderPath = path.join(__dirname, './src') // 文件夹路径
+const folderPath = path.join(__dirname, './') // 文件夹路径
 const folderName = folderPath.substr(folderPath.lastIndexOf('\\')+1) // 文件夹名称
-const minSize = 500 // 要显示的最小的文件(kb)
+const minSize = 0 // 要显示的最小的文件(kb)
 const fileName = 'size.txt' // 生产的文件名称
+let extname = ['jpg','png','js'] //遍历指定拓展名的文件
 
 // 遍历所有文件夹，获取文件信息 
-function geFileList(path) {
+function geFileList(filePath) {
   let filesList = []
   try {
-    readFile(path, filesList)
+    readFile(filePath, filesList)
   } catch (error) {
     throw new Error(error)
   }
@@ -21,18 +21,24 @@ function geFileList(path) {
 }
 
 //遍历读取文件 
-function readFile(path, filesList) {
-  let files = fs.readdirSync(path) //需要用到同步读取 
+function readFile(filePath, filesList) {
+  let files = fs.readdirSync(filePath) //需要用到同步读取
   files.forEach(file => {
-    let states = fs.statSync(path + '/' + file)
+    let states = fs.statSync(filePath + '/' + file)
+    // 文件夹
     if (states.isDirectory()) {
-      readFile(path + '/' + file, filesList)
+      readFile(filePath + '/' + file, filesList)
       return
     }
+    // 后缀名
+    let ext = path.extname(file).toLowerCase()
+    // 过滤后缀名
+    if (extname && (!ext || extname.indexOf(ext) === -1)) return
+
     filesList.push({
       name: file, //文件名
       size: states.size, //文件大小，以字节为单位
-      path: path + '/' + file //文件绝对路径
+      path: filePath + '/' + file //文件绝对路径
     })
   })
 }
@@ -47,8 +53,9 @@ function filerList(list) {
     count++
     data += `size: ${size}/kb  path: ${folderName + item.path.replace(folderPath, '')} \n`
   }
-  data = `文件路径：${folderPath}\n文件总数量：${list.length}\n>=${minSize}/kb文件数量：${count}\n` + data
-  return data
+  let totalStr = `文件路径：${folderPath}\n文件总数量：${list.length}\n`
+  let minStr = minSize ?`>=${minSize}/kb文件数量：${count}\n`: ''
+  return totalStr + minStr + data
 }
 
 // 写入文件utf-8格式
@@ -57,6 +64,10 @@ function writeFile(data) {
 }
 
 (() => {
+
+  extname = extname.map(item => {
+    return `.${item.toLowerCase()}`
+  }).join('')
 
   const filesList = geFileList(folderPath)
   // 过滤数组
